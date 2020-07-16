fd5b65b94e81bd6c682fd8cbc5b5d1ea4c0e5726
JimJeon <jimjeon.me@gmail.com>
2020-07-16 13:05:15 +0900

    Fix formats with clang-format using Microsoft style

--- Source/TheShelters/Monster.cpp
+++ Source/TheShelters/Monster.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Monster.h"
 
 // constructor sets monster characteristics and its id. depends on MonsterType t
@@ -8,7 +7,8 @@ void UMonster::InitMonster(MonsterType t, int id)
 {
     monsterId = id;
 
-    if (t == DefaultMonster) {
+    if (t == DefaultMonster)
+    {
         radioactive = false;
         emp = false;
         armorpierce = false;
@@ -16,10 +16,24 @@ void UMonster::InitMonster(MonsterType t, int id)
     }
 }
 
-UMonster::UMonster() {}
+UMonster::UMonster()
+{
+}
 // returns monsterId. Id for individual instances of monsters
-const int             UMonster::MonsterId() const { return monsterId; }
+const int UMonster::MonsterId() const
+{
+    return monsterId;
+}
 // returns monsterType. type of monsters can be same among individuals
-const MonsterType     UMonster::Type()      const { return monsterType; }
-const int             UMonster::Speed()     const { return speed; }
-const MonsterProperty UMonster::Property()  const { return MakeTuple(radioactive, emp, armorpierce); }
+const MonsterType UMonster::Type() const
+{
+    return monsterType;
+}
+const int UMonster::Speed() const
+{
+    return speed;
+}
+const MonsterProperty UMonster::Property() const
+{
+    return MakeTuple(radioactive, emp, armorpierce);
+}
