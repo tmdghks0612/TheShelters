40b2cb558234bbfa9969b66951b9877557157a73
tmdghks0612 <dreamspidersh@gmail.com>
2020-07-20 18:42:59 +0900

    random room gen complete
    random room generation complete on RoomActor.cpp
    
    ZapCCTV reworked to zap where monsters move

---
+++ Source/TheShelters/RoomActor.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "RoomActor.generated.h"
+
+UCLASS()
+class THESHELTERS_API ARoomActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ARoomActor();
+
+	UFUNCTION(BlueprintCallable)
+	void RoomMeshRandomize();
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RoomRoot")
+	USceneComponent* Root;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RoomMesh")
+	UStaticMeshComponent* RoomMeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RoomMesh")
+    TArray<UStaticMesh *> RoomMeshVersions;
+
+};
