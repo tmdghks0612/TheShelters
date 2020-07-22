855b48b091a0ee006df01454f6bba9e23a76dafd
JimJeon <jimjeon.me@gmail.com>
2020-07-22 14:38:49 +0900

    Add PlayerStat class

--- Source/TheShelters/Survivor.cpp
+++ Source/TheShelters/Survivor.cpp
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include <algorithm>
 
 #include "RobotControl.h"
 #include "Survivor.h"
@@ -10,13 +11,12 @@
 #include "Components/StaticMeshComponent.h"
 #include "Components/InputComponent.h"
 
-
 // Sets default values
 ASurvivor::ASurvivor()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	
+
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
 
 	SpringArmComp->SetupAttachment(RootComponent);
@@ -36,59 +36,157 @@ ASurvivor::ASurvivor()
 void ASurvivor::BeginPlay()
 {
 	Super::BeginPlay();
+
+	InitPlayerStat();
 }
 
-void ASurvivor::TurnView() {
-	if (!Controller) {
+void ASurvivor::TurnView()
+{
+	if (!Controller)
+	{
 		UE_LOG(LogTemp, Warning, TEXT("no controller!"));
 		return;
 	}
-	
+
 	AddControllerYawInput(360.0f);
 
 	return;
 }
 
 // Called to bind functionality to input
-void ASurvivor::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ASurvivor::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAction("TurnView", IE_Pressed, this, &ASurvivor::TurnView);
-    PlayerInputComponent->BindAction("CameraChangeToMap", IE_Pressed, this, &ASurvivor::CameraChange);
+	PlayerInputComponent->BindAction("CameraChangeToMap", IE_Pressed, this, &ASurvivor::CameraChange);
 
 	PlayerInputComponent->BindAction("MapRight", IE_Pressed, this, &ASurvivor::RobotMapRight);
-    PlayerInputComponent->BindAction("MapLeft", IE_Pressed, this, &ASurvivor::RobotMapLeft);
-    PlayerInputComponent->BindAction("MapUp", IE_Pressed, this, &ASurvivor::RobotMapUp);
-    PlayerInputComponent->BindAction("MapDown", IE_Pressed, this, &ASurvivor::RobotMapDown);
+	PlayerInputComponent->BindAction("MapLeft", IE_Pressed, this, &ASurvivor::RobotMapLeft);
+	PlayerInputComponent->BindAction("MapUp", IE_Pressed, this, &ASurvivor::RobotMapUp);
+	PlayerInputComponent->BindAction("MapDown", IE_Pressed, this, &ASurvivor::RobotMapDown);
 }
 
 void ASurvivor::CameraChange()
 {
-
 }
 
 void ASurvivor::InitRobots(ARobotControl *_Robot)
 {
-    Robot = _Robot;
+	Robot = _Robot;
 }
 
 void ASurvivor::RobotMapRight()
 {
-    Robot->MapRight();
+	Robot->MapRight();
 }
 
 void ASurvivor::RobotMapLeft()
 {
-    Robot->MapLeft();
+	Robot->MapLeft();
 }
 
 void ASurvivor::RobotMapUp()
 {
-    Robot->MapUp();
+	Robot->MapUp();
 }
 
 void ASurvivor::RobotMapDown()
 {
-    Robot->MapDown();
+	Robot->MapDown();
+}
+
+UPlayerStat::UPlayerStat() {}
+UPlayerStat::~UPlayerStat() {}
+
+void UPlayerStat::InitPlayerStat()
+{
+	this->food = 100;
+	this->water = 100;
+	this->mental = 50;
+	this->progress = 0;
+	this->electricity = 100;
+}
+
+const int UPlayerStat::Food() const
+{
+	return this->food;
+}
+
+const int UPlayerStat::Water() const
+{
+	return this->water;
+}
+
+const int UPlayerStat::Mental() const
+{
+	return this->mental;
+}
+
+const int UPlayerStat::Progress() const
+{
+	return this->progress;
+}
+
+const int UPlayerStat::Electricity() const
+{
+	return this->electricity;
+}
+
+const int UPlayerStat::Food(const int diff)
+{
+	this->food += diff;
+	this->food = std::max(0, this->food);
+	this->food = std::min(this->food, this->max);
+	return this->food;
+}
+
+const int UPlayerStat::Water(const int diff)
+{
+	this->water += diff;
+	this->water = std::max(0, this->water);
+	this->water = std::min(this->water, this->max);
+	return this->water;
+}
+const int UPlayerStat::Mental(const int diff)
+{
+	this->mental += diff;
+	this->mental = std::max(0, this->mental);
+	this->mental = std::min(this->mental, this->max);
+	return this->mental;
+}
+const int UPlayerStat::Progress(const int diff)
+{
+	this->progress += diff;
+	this->progress = std::max(0, this->progress);
+	this->progress = std::min(this->progress, this->max);
+	return this->progress;
+}
+const int UPlayerStat::Electricity(const int diff)
+{
+	this->electricity += diff;
+	this->electricity = std::max(0, this->electricity);
+	this->electricity = std::min(this->electricity, this->max);
+	return this->electricity;
+}
+
+void UPlayerStat::Consume()
+{
+	int foodPerTime = 1;
+	int waterMultiplier = 2;
+	Food(-foodPerTime);
+	Water(-foodPerTime * waterMultiplier);
+}
+
+void UPlayerStat::TimePass()
+{
+	// Consume food and water
+	Consume();
+
+	// Change mental
+	int mentalDiff = 1 * mentalMultiplier;
+	Mental(mentalDiff);
+
+	// Consume electricity
+	Electricity(-electricityUsage);
 }
--- Source/TheShelters/Survivor.h
+++ Source/TheShelters/Survivor.h
@@ -23,33 +23,31 @@ public:
 	void CameraChange();
 
 	UFUNCTION(BlueprintCallable)
-    void InitRobots(ARobotControl* _Robot);
+	void InitRobots(ARobotControl *_Robot);
 
 	UFUNCTION()
-    void RobotMapRight();
-    UFUNCTION()
-    void RobotMapLeft();
-    UFUNCTION()
-    void RobotMapUp();
-    UFUNCTION()
-    void RobotMapDown();
-
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
-	USpringArmComponent* SpringArmComp;
+	void RobotMapRight();
+	UFUNCTION()
+	void RobotMapLeft();
+	UFUNCTION()
+	void RobotMapUp();
+	UFUNCTION()
+	void RobotMapDown();
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
-	UCameraComponent* CameraComp;
+	USpringArmComponent *SpringArmComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	UCameraComponent *CameraComp;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player")
-	UStaticMeshComponent* MeshComp;
+	UStaticMeshComponent *MeshComp;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-    ARobotControl* Robot = nullptr;
+	ARobotControl *Robot = nullptr;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-    TArray<ARobotControl *> lists;
-
+	TArray<ARobotControl *> lists;
 
 protected:
 	// Called when the game starts or when spawned
@@ -60,9 +58,51 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	float BaseTurnRate;
 
-public:	
-
+public:
 	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
+	virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
+
+private:
+	unsigned int playerStat;
 };
+
+UCLASS()
+class THESHELTERS_API UPlayerStat : public UObject
+{
+	GENERATED_BODY()
+public:
+	// Constructors and Initializers
+	UPlayerStat();
+	~UPlayerStat();
+	void InitPlayerStat();
+
+	// Getters
+	const int Food() const;
+	const int Water() const;
+	const int Mental() const;
+	const int Progress() const;
+	const int Electricity() const;
+
+	const int Food(const int diff);
+	const int Water(const int diff);
+	const int Mental(const int diff);
+	const int Progress(const int diff);
+	const int Electricity(const int diff);
+
+	// Methods for playing game
+	void TimePass();
+
+private:
+	int max = 100;
+	int food;
+	int water;
+	int mental;
+	int progress;
+	int electricity;
+
+	int mentalMultiplier;
+	int electricityUsage;
+
+	// Private methods for playing game
+	void Consume();
+}
